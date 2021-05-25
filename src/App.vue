4c8d05318b8b5a97f255a51179bd8e80c2d60e4c
王文堃 <wangwk-a@glodon.com>
2021-05-25 14:42:34 +0800

    添加基本框架

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+      <Home></Home>
+  </div>
+</template>
+
+<script>
+import Home from "@/components/Home/Home";
+export default {
+  name: 'App',
+  components: {
+      Home
+  }
+}
+</script>
+
+<style>
+#app {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+</style>
